06b12758efebb6f632bfc24374b5116983dca720
Roman <dev@rman.dev>
2026-06-24 18:45:14 +0100

    format, fix some linter errors

--- src/components/CsvUpload.tsx
+++ src/components/CsvUpload.tsx
@@ -88,6 +88,7 @@ export default function CsvUpload(props: CsvUploadProps) {
           stroke="currentColor"
           stroke-width="2"
           class="text-gray-400"
+          aria-hidden="true"
         >
           <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
           <polyline points="17 8 12 3 7 8"></polyline>
